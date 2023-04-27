56ec76f17e7414a68e0b107046be887c7b549855
Anthony Keith <jt1keith@gmail.com>
2023-04-27 11:31:35 -0700

    portfolio redesign working as intended

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+    <div id="content">
+      <h1>Contact me</h1>
+      <p>You can reach me at john.doe@example.com.</p>
+    </div>
+  );
+}
+
+export default Contact;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,74 +1,14 @@
-import React, { Component } from 'react';
-import data from '../images/data.json';
-
-import './Projects.css';
-
-class Projects extends Component {
-  state = {
-    activeProjectIndex: 0
-  }
-
-  setActiveProject = (index) => {
-    this.setState({ activeProjectIndex: index });
-  }
-
-  render() {
-    const activeProject = data[this.state.activeProjectIndex];
-
-    return (
-      <>
-        <div className='stats'>
-          <div className='filler project'>
-        <div className='project-buttons'>
-          {data.map((project, index) => (
-            <button
-              key={project.title}
-              onClick={() => this.setActiveProject(index)}
-              className={index === this.state.activeProjectIndex ? 'active project-button-list' : 'project-button-list'}
-            >
-              <h4 className='project-title'>
-
-              {project.title}
-              </h4>
-            </button>
-          ))}
-        </div>
-          </div>
-        </div>
-        <div className='projects'>
-          {activeProject && <ProjectItem project={activeProject} />}
-        </div>
-      </>
-    );
-  }
+function Projects() {
+  return (
+    <div id="content">
+      <h1>My projects</h1>
+      <ul>
+        <li>Project A</li>
+        <li>Project B</li>
+        <li>Project C</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Projects;
-
-const ProjectItem = ({ project }) => {
-  return (
-    <div className='hero'>
-      <div className='project-image'>
-        <img src={project.imageSrc} alt={project.imageAlt} />
-      </div>
-      <div className='project-details'>
-        <h2>{project.title}</h2>
-        <p>{project.description}</p>
-        <div className='project-links'>
-          {project.githubLink && (
-            <a href={project.githubLink} target='_blank' rel='noopener noreferrer'>
-            Github
-            </a>
-          )}
-          {project.liveLink && (
-            <a href={project.liveLink} target='_blank' rel='noopener noreferrer'>
-              Live
-            </a>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-
