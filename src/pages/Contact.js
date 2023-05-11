3bf085c58d0efc6e84c9de7b30b32b17b4a8d03b
Anthony Keith <jt1keith@gmail.com>
2023-05-10 19:11:42 -0700

    home page working

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,12 @@
+import './Contact.css'
+
 function Contact() {
   return(
-    <div className='animate__animated animate__rotateInUpLeft' id="content">
-    <h1>Contact me</h1>
-    </div>
+    <>
+    <div className='animate__animated animate__rotateInUpLeft contact' id="content">
+    <h5>Contact Me!</h5>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd0HvhnZpG-Xn40-NlsxLnLiKbCnREug_f1cLb8JXPAXJ1GxQ/viewform?embedded=true" width="600" height="900" frameborder="0" marginheight="0" marginwidth="0" title="contact">Loading…</iframe>    </div>
+    </>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
+import HomeImage from '../home_image.png';
+import './Home.css'
+
 function Home() {
   return (
-    <div className='animate__animated animate__rotateInUpRight' id="content">
-      <h1>Hello!</h1>
-      <p>My name is Anthony Keith, a Front End Developer and UX designer with a focus on creating engaging and interactive user experiences that increase brand awareness. My approach involves utilizing innovative techniques such as animation, microinteractions, and other design elements to craft memorable user experiences that establish a lasting connection between the brand and its audience. By placing the user's needs and desires at the forefront of my designs, I strive to create effective interfaces that encourage user engagement.</p>
+    <div className='animate__animated animate__rotateInUpRight home-content' id="content">
+      <div className='home-text'>
+      <h1>Welcome to my portfolio!</h1>
+      <p>My name is Anthony Keith, a Front End Developer and UX designer with a focus on creating engaging user interfaces that increase brand awareness. My approach utilizes techniques such as animation, microinteractions, and AI to craft memorable user experiences that establish a lasting connection between the brand and its audience.</p>
+      </div>
+      <img src={HomeImage} alt="AI generated art of man coding" className="home-image" title='Midjourney generated image of me coding'/>
     </div>
   );
 }
 
 export default Home;
-
-/*
-Hello! My name is Anthony Keith, a Front End Developer and UX designer with a focus on creating engaging and interactive user experiences that increase brand awareness. My approach involves utilizing innovative techniques such as animation, microinteractions, and other design elements to craft memorable user experiences that establish a lasting connection between the brand and its audience. By placing the user's needs and desires at the forefront of my designs, I strive to create effective interfaces that encourage user engagement.
-
-*/
