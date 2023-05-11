9b2ac296aceebada89bb4573f7366a5af681a943
Anthony Keith <jt1keith@gmail.com>
2023-05-10 20:46:39 -0700

    grid implemented

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,30 @@
+import './About.css'
+
 function About({ animationDirection }) {
-
   return (
-    <div className={`animate__animated animate__rotateInUp${animationDirection}`} id="content">
-
-      <h1>About me:</h1>
-      <p>I'm the kind of person who dives head first into new situations, whether its taking on a new framework, exploring a new city, or trying out a new hobby. I enjoy bold eye-catching colors to create interfaces that stand out and capture the attention of anyone who sees it. When I'm not coding, I indulge in my love for cooking and experimenting with cooking and experimenting with new culinary creations. Prior to my career in tech, I served in the Air Force, where I developed valuable skills such as attention to detail, discipline, and teamwork.</p>
+    <div className={`animate__animated animate__rotateInUp${animationDirection} about-me`}>
+      <header>
+      <h5>More About me:</h5>
+      </header>
+      <span>
+        <h3>As a developer:</h3>
+        <p>I'm the kind of person who dives head first into new situations, whether it's taking on a new framework, exploring a new city, or trying out a new hobby. I enjoy bold eye-catching colors to create interfaces that stand out and capture the attention of anyone who sees it. When I'm not coding, I indulge in my love for cooking and experimenting with cooking and experimenting with new culinary creations. Prior to my career in tech, I served in the Air Force, where I developed valuable skills such as attention to detail, discipline, and teamwork.</p>
+      </span>
+      <aside>
+        <h3>Tools I Know</h3>
+        <p>Here are some of the tools and technologies I'm familiar with:</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>Git</li>
+          <li>Photoshop</li>
+        </ul>
+      </aside>
     </div>
   );
 }
 
 export default About;
-
-/*
-About Me:
-
-I'm the kind of person who dives head first into new situations, whether its taking on a new framework, exploring a new city, or trying out a new hobby. I enjoy bold eye-catching colors to create interfaces that stand out and capture the attention of anyone who sees it. When I'm not coding, I indulge in my love for cooking and experimenting with cooking and experimenting with new culinary creations. Prior to my career in tech, I served in the Air Force, where I developed valuable skills such as attention to detail, discipline, and teamwork.
-
-See More...
-
-Aside from my work as a React Developer, I enjoy perfecting my aim with Ana playing Overwatch or going to trivia nights with my friends. A life goal of mine is to step foot on in 100 unique countries, and I'm happy to say so far I am at 7! I believe experiencing the world first hand is one of the most rewarding things a person can do, and I'm excited to see where my adventures take me.
-*/
